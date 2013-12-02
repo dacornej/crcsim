de497272887de46e19faae21bedc8bd29f7a0379
dacornej <david.a.cornejo@gmail.com>
2013-12-02 09:58:57 -0500

    Add itterations functionality

--- bin/compile-output.py
+++ bin/compile-output.py
@@ -24,24 +24,25 @@ for root, dirname, filename in os.walk (input_dir):
             stream = open (path, 'r')
             if not header_output:
                 header = stream.readline ()
-                out_stream.write ("age\tintervention\treplication\t")
+                out_stream.write ("age\tintervention\titeration\treplication\t")
                 out_stream.write (header)
                 header_output = True
             else:
                 stream.readline ()
 
             age = path.split ('/')[-2].split ('.')[2]
+            iteration = path.split('/')[-2].split('.')[3]
             # NOTE From meeting: should probaly add itteration here with similar syntax to lines above and below
             scenario = path.split ('/')[-2].split ('.')
 
             ''' account for control vs intervention format. '''
-            intervention = scenario [4] if len(scenario) > 4 else scenario [3]
+            intervention = scenario [5] if len(scenario) > 5 else scenario [4]
 
             replication = path.split ('/')[-1].split ('.')[2]
             if 'control' == intervention:
                 intervention = -1
 
-            out_stream.write ("%s\t%s\t%s\t" % (age, intervention, replication))
+            out_stream.write ("%s\t%s\t%s\t%s\t" % (age, intervention, iteration, replication))
             out_stream.write (stream.readline ())
         finally:
             if stream:
